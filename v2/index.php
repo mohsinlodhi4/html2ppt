a5c2759e7fb872c1f0ed3250716f7261390ceb9d
Mohsin Khan Lodhi <mohsinlodhi4@gmail.com>
2022-09-21 22:23:38 -0700

    half

--- v2/index.php
+++ v2/index.php
@@ -10,6 +10,7 @@ use PhpOffice\PhpPresentation\IOFactory;
 use PhpOffice\PhpPresentation\Style\Color;
 use PhpOffice\PhpPresentation\Style\Border;
 use PhpOffice\PhpPresentation\Style\Alignment;
+use PhpOffice\PhpPresentation\Style\Fill;
 
 // echo "<pre>";
 // print_r( get_class_methods(PhpPresentation::class) );
@@ -18,35 +19,61 @@ use PhpOffice\PhpPresentation\Style\Alignment;
 
 $data = [
     [ // slide1
-        'logo'=>  './logo.png',
-        'h1'=> 'Parabéns, sua avaliação está pronta! ',
-        'h2' => ' Perfil',
-        'p2' => ' Empresa Teste Ltda - Santa Cruz do Sul, RS
-Fundada em 2020',
-        'h3'=>'Operações',
-        'p3'=>['text'=>'Estágio da empresa: Crescimento inicial 
+        ['logo'=>  './logo.png'],
+        ['h1'=> 'Parabéns, sua avaliação está pronta! '],
+        ['h2' => ' Perfil'],
+        ['p2' => ' Empresa Teste Ltda - Santa Cruz do Sul, RS
+Fundada em 2020'],
+        ['h3'=>'Operações'],
+        ['p3'=>['text'=>'Estágio da empresa: Crescimento inicial 
 Número de sócios: 2 
 Experiência da equipe: Mais de 10 anos
-Propriedades intelectuais: Marca registrada, Certificações, Domínio / Website / Softwares', 'lines'=>4],
-        'h4'=>'Oportunidade',
-        'p4'=>'Média concorrência
-Negócio não escalável',
-        'table'=> [
+Propriedades intelectuais: Marca registrada, Certificações, Domínio / Website / Softwares', 'lines'=>4]],
+        ['h4'=>'Oportunidade'],
+        ['p4'=>'Média concorrência
+Negócio não escalável'],
+        ['table'=> [
                 ['SETOR', 'EMPRESAS', 'D/E'],
                 ['Software (Internet)', '40', '15.22%'],
                 ['Software (Sistemas e Aplicações)', '462', '4.43%'],
-        ],
-        'center'=>  'Beta dos setores: 1.28 β'
+        ]],
+        ['center'=>  'Beta dos setores: 1.28 β']
     ],
 
     [ // slide2
-        'h1'=> 'Desempenho ',
-        'p1' => ['lines'=>1,'text'=>'Receita                                                  297.000,00 R$'],
-        'p2' => ['lines'=>1,'text'=>'Dívidas                                                  15.000,00 R$'],
-        'p3' => ['lines'=>1,'text'=>'Dinheiro em caixa                                        20.000,00 R$'],
-        'p4' => ['lines'=>1,'text'=>'Retorno Sobre o Capital Investido (ROIC)                 80,00 R$'],
-        'p5' => ['lines'=>1,'text'=>'EBIT                                                     84.800,00 R$'],
-        'p6' =>      ['lines'=>1,'text'=>'Margem EBIT                                              28.55%'],
+        ['h1'=> 'Desempenho '],
+        ['p1' => ['lines'=>1,'text'=>'Receita                                                  297.000,00 R$']],
+        ['p2' => ['lines'=>1,'text'=>'Dívidas                                                  15.000,00 R$']],
+        ['p3' => ['lines'=>1,'text'=>'Dinheiro em caixa                                        20.000,00 R$']],
+        ['p4' => ['lines'=>1,'text'=>'Retorno Sobre o Capital Investido (ROIC)                 80,00 R$']],
+        ['p5' => ['lines'=>1,'text'=>'EBIT                                                     84.800,00 R$']],
+        ['p6' =>      ['lines'=>1,'text'=>'Margem EBIT                                              28.55%']],
+        [ 'table'=> [
+            [ ['Taxa Livre de Risco', '12.56%'], ['Prêmio de Risco de Capital Próprio', '7.21%'], ['Desconto de Iliquidez', '15%'] ],
+            [ ['Risco País', '2.97%'], ['Custo do Capital', '19,28%'], ['Custo da Dívida', '15,88%'] ],
+            [ ['Custo do Capital Próprio', '19,75%'], ['IPCA acumulado 12 meses', '11.30%'], ['Cotação diária do dólar', 'R$ 5,13'] ],
+        ] ],
+        [ 'h1'=> '1. Valor econômico com DCF' ],
+        [ 'p1'=> ['text'=>'No DCF (Discounted Cash Flow, ou Fluxo de Caixa Descontado), o objetivo é identificar o valor de uma empresa considerando seu fluxo de caixa, crescimento e risco. Ou seja, estima o seu potencial econômico em gerar retorno para sócios e credores. Esse critério engloba fatores como plano de negócio, competência da gestão, tradição, marca, carteira de clientes e qualquer decisão que impacte na geração de resultados.', 'lines'=>6] ],
+        [ 'p1'=>['text'=>'Conforme Aswath Damodaran, uma boa avaliação é o resultado de dois elementos: o desempenho histórico da empresa e a criação de uma narrativa sólida (através das projeções financeiras).', 'lines'=>3] ],
+    ],
+
+    [ // slide3
+        ['centerBox'=> ['text'=>'Desempenho', 'text2'=> 'R$ 718.740,00', 'text2Font'=>20, 'bg'=>'FFd7f5fc', 'text2Color'=>'FF03c3ec', 'width'=>600 ]],
+        ['p1' => ['lines'=>1,'text'=>'Receita                                                  297.000,00 R$']],
+        ['p2' => ['lines'=>1,'text'=>'Dívidas                                                  15.000,00 R$']],
+        ['p3' => ['lines'=>1,'text'=>'Dinheiro em caixa                                        20.000,00 R$']],
+        ['p4' => ['lines'=>1,'text'=>'Retorno Sobre o Capital Investido (ROIC)                 80,00 R$']],
+        ['p5' => ['lines'=>1,'text'=>'EBIT                                                     84.800,00 R$']],
+        ['p6' =>      ['lines'=>1,'text'=>'Margem EBIT                                              28.55%']],
+        [ 'table'=> [
+            [ ['Taxa Livre de Risco', '12.56%'], ['Prêmio de Risco de Capital Próprio', '7.21%'], ['Desconto de Iliquidez', '15%'] ],
+            [ ['Risco País', '2.97%'], ['Custo do Capital', '19,28%'], ['Custo da Dívida', '15,88%'] ],
+            [ ['Custo do Capital Próprio', '19,75%'], ['IPCA acumulado 12 meses', '11.30%'], ['Cotação diária do dólar', 'R$ 5,13'] ],
+        ] ],
+        [ 'h1'=> '1. Valor econômico com DCF' ],
+        [ 'p1'=> ['text'=>'No DCF (Discounted Cash Flow, ou Fluxo de Caixa Descontado), o objetivo é identificar o valor de uma empresa considerando seu fluxo de caixa, crescimento e risco. Ou seja, estima o seu potencial econômico em gerar retorno para sócios e credores. Esse critério engloba fatores como plano de negócio, competência da gestão, tradição, marca, carteira de clientes e qualquer decisão que impacte na geração de resultados.', 'lines'=>6] ],
+        [ 'p1'=>['text'=>'Conforme Aswath Damodaran, uma boa avaliação é o resultado de dois elementos: o desempenho histórico da empresa e a criação de uma narrativa sólida (através das projeções financeiras).', 'lines'=>3] ],
     ],
 
 ];
@@ -64,7 +91,9 @@ function createPpt($slides, $title='slide'){
         $currentSlide = $i==0 ? $objPHPPowerPoint->getActiveSlide() : $objPHPPowerPoint->createSlide();
 
         $currentYOffset = 0;        
-        foreach($slide as $tag => $value){
+        foreach($slide as $element){
+            $tag = array_key_first($element);
+            $value = $element[$tag];
 
             if($tag=='logo'){
                 $currentYOffset += 10; 
@@ -134,36 +163,46 @@ function createPpt($slides, $title='slide'){
 
             }
 
+            if($tag=='centerBox'){
+                $currentYOffset += 5; 
+                $height = 40;
+                $width = $value['width'];
+                $text = $value['text'];
+                // if(is_array($value)){
+                //     $height = 25*$value['lines'];
+                //     $value = $value['text'];
+                // }
+                
+                $fontSize = 14; 
+                $shape = $currentSlide->createRichTextShape()
+                ->setHeight($height)
+                ->setWidth($width)
+                ->setOffsetX(150)
+                ->setOffsetY($currentYOffset);
+                $shape->getActiveParagraph()->getAlignment()->setHorizontal( Alignment::HORIZONTAL_CENTER );
+                $textRun = $shape->createTextRun($text);
+                $textRun->getFont()
+                ->setSize($fontSize)
+                ->setColor( new Color( 'FF000000' ) );
+                $bgColor = new Color($value['bg']);
+                $shape->getFill()->setFillType(Fill::FILL_SOLID)
+                    ->setStartColor($bgColor)
+                    ->setEndColor($bgColor);
+            }
+
             if($tag == 'table'){
-                $currentYOffset += 5;
+                $currentYOffset += 15;
                 $height = sizeof($value)*40;
+                if( is_array($value[0][0]) ){
+                    $height = sizeof($value)*50;
+                }
                 $columns = $value[0];
                 $shape = $currentSlide->createTableShape( sizeof($columns) );
                 $shape->setOffsetY($currentYOffset)
                 ->setOffsetX(10)
                 ->setWidth(800);
 
-                $j=0;
-                foreach($value as $rowData){
-                    $row = $shape->createRow();
-                    foreach($rowData as $cellValue){
-                        $cell = $row->nextCell();
-                        $textRun = $cell->createTextRun($cellValue);
-                        $textRun->getFont()->setBold(true);
-                        $textRun->getFont()->setSize(12);
-                        $textRun->getFont()->setColor(new Color( $j==0 ? 'FF47008a' : 'FF000000'));
-                        // $cell->createBreak();
-                        // $textRun = $cell->createTextRun('Multiline');
-                        // $textRun->getFont()->setBold(true);
-                        // $textRun->getFont()->setSize(14);
-                        // $textRun->getFont()->setColor(new Color('FF0088FF'));
-
-                        $cell->getActiveParagraph()->getAlignment()
-                        ->setMarginLeft(10);
-                    }
-                    $j++;
-                }
-
+                createTable($shape, $value);
             }
 
             $currentYOffset += $height;
@@ -176,4 +215,35 @@ function createPpt($slides, $title='slide'){
     $oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
     $oWriterPPTX->save(__DIR__ . "/$title.pptx");
     
+}
+
+function createTable($shape, $table){
+    $j=0;
+
+    $isTwoDimenional = is_array($table[0][0]);
+    foreach($table as $rowData){
+        $row = $shape->createRow();
+        foreach($rowData as $cell){
+            $cellValue = $isTwoDimenional ? $cell[0] : $cell;
+            $secondLine = $isTwoDimenional ? $cell[1] : '';
+            $colorHex = $j==0 && !$isTwoDimenional ? 'FF47008a' : 'FF000000';
+
+            $cell = $row->nextCell();
+            $textRun = $cell->createTextRun($cellValue);
+            $textRun->getFont()->setBold(true);
+            $textRun->getFont()->setSize(12);
+            $textRun->getFont()->setColor(new Color( $colorHex ));
+            if($isTwoDimenional){
+                $cell->createBreak();
+                $textRun = $cell->createTextRun($secondLine);
+                $textRun->getFont()->setBold(true);
+                $textRun->getFont()->setSize(14);
+                $textRun->getFont()->setColor(new Color('FF47008a'));
+            }
+
+            $cell->getActiveParagraph()->getAlignment()
+            ->setMarginLeft(10)->setMarginBottom(5);
+        }
+        $j++;
+    }
 }
